c83178b7e252772c38c116e875256d7e4daa1b9b
zeeshanhanif <zeeshanhanif@gmail.com>
2021-11-18 00:41:56 +0500

    Removed chainlink request function and updated BondNFT creation process

--- contracts/BondNFTGenerator.sol
+++ contracts/BondNFTGenerator.sol
@@ -6,11 +6,20 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./BondNFT.sol";
 
 contract BondNFTGenerator is Ownable {
-    function generateNFT(string memory _artistName, string memory _artistId, string memory _channelId, 
-                string memory _endpoint, string memory _audiusArtistId, uint256 _fundingAmount, 
-                uint256 _numberOfYears, uint256 _numberOfBonds, uint256 _facevalue) public returns(address) {
-        BondNFT nft = new BondNFT(_artistName, _artistId, _channelId, _endpoint, _audiusArtistId,
-                        _fundingAmount, _numberOfYears, _numberOfBonds, _facevalue);
+
+    function generateNFT(string memory _bondName, string memory _bondSymbol) public returns(address) {
+        BondNFT nft = new BondNFT(_bondName, _bondSymbol);
         return address(nft);
     }
+    /*
+    function generateNFT(string memory _artistName, string memory _artistId, string memory _channelId, 
+                string memory _endpoint, string memory _audiusArtistId, uint256 _fundingAmount, 
+                uint256 _numberOfYears, uint256 _numberOfBonds, uint256 _facevalue, 
+                address _chainlinkOracleInfoAddress, string memory _bondName, string memory _bondSymbol) public returns(address) {
+        
+        BondNFT nft = new BondNFT(_artistName, _artistId, _channelId, _endpoint, _audiusArtistId,
+                        _fundingAmount, _numberOfYears, _numberOfBonds, _facevalue, _chainlinkOracleInfoAddress,_bondName, _bondSymbol);
+        return address(nft);
+        
+    }*/
 }
--- contracts/BondNFTManager.sol
+++ contracts/BondNFTManager.sol
@@ -1,27 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "./RatingEngine.sol";
 import "./BondNFTGenerator.sol";
+import "./ChainlinkOracleInfo.sol";
+import "./BondNFT.sol";
 
-contract BondNFTManager is ChainlinkClient, Ownable {
+contract BondNFTManager is Ownable {
 
     using Strings for string;
-    using Chainlink for Chainlink.Request;
-
-    address private oracle;
-    bytes32 private jobId;
-    uint256 private fee;
 
     string private defaultEndpont = "channel";
     RatingEngine private ratingEngine;
     BondNFTGenerator private bondNFTGenerator;
-
-    event ListenerRequestInitiated(bytes32 indexed _requestId);
-    event RequestListenerFulfilled(bytes32 indexed _requestId, uint256 indexed _listeners, address indexed nftAddress);
+    ChainlinkOracleInfo private chainlinkOracleInfo;
 
     struct BondConfig {
         string artistName;
@@ -33,66 +27,44 @@ contract BondNFTManager is ChainlinkClient, Ownable {
         uint256 numberOfYears;
         uint256 numberOfBonds;
         address issuerAddress;
-        bytes32 requestId;
         uint256 faceValue;
         uint256 listeners;
+        address nftAddress;
     }
 
-    mapping(bytes32 => BondConfig) private bondConfigs;
-    mapping(bytes32 => address) requestToNFTAddress;
+    mapping(address => BondConfig[]) private userBondConfigs;
+    address[] public bondNfts;
     
-
     constructor() {   
-        setPublicChainlinkToken();
     }
 
-    function initialize(address _oracle, string memory _jobId, address _ratingEngine, address _bondNftGenerator) public onlyOwner {
-        oracle = _oracle;
-        jobId = stringToBytes32(_jobId);
-        fee = 1 * 10 ** 18; // (Varies by network and job)
+    function initialize(address _ratingEngine, address _bondNftGenerator, address _chainlinkOracleInfoAddress) public onlyOwner {
         ratingEngine = RatingEngine(_ratingEngine);
         bondNFTGenerator = BondNFTGenerator(_bondNftGenerator);
+        chainlinkOracleInfo = ChainlinkOracleInfo(_chainlinkOracleInfoAddress);
     }
 
-    function issueBond(string memory _artistName, string memory _artistId, string memory _channelId, string memory _audiusArtistId, uint256 _fundingAmount, uint256 _numberOfYears, uint256 _numberOfBonds, uint256 _facevalue) public {
-        bytes32 requestId = requestChannelListeners(_channelId, defaultEndpont);
-        BondConfig memory _config = BondConfig(_artistName,_artistId,_channelId,defaultEndpont,_audiusArtistId,_fundingAmount, _numberOfYears, _numberOfBonds, msg.sender,requestId,_facevalue,0);
-        bondConfigs[requestId] = _config;
+    function issueBond(string memory _artistName, string memory _artistId, string memory _channelId, string memory _audiusArtistId, uint256 _fundingAmount, uint256 _numberOfYears, uint256 _numberOfBonds, uint256 _facevalue, string memory _bondName, string memory _bondSymbol) public {
+        
+        address nftAddress = bondNFTGenerator.generateNFT(_bondName, _bondSymbol);
+        BondNFT bondNFT = BondNFT(nftAddress);
+        bondNFT.initialize(_artistName, _artistId, _channelId, defaultEndpont, _audiusArtistId, _fundingAmount, _numberOfYears, _numberOfBonds, _facevalue, address(chainlinkOracleInfo));
+        
+        BondConfig memory _config = BondConfig(_artistName,_artistId,_channelId,defaultEndpont,_audiusArtistId,_fundingAmount, _numberOfYears, _numberOfBonds, msg.sender,_facevalue,0,nftAddress);
+        userBondConfigs[msg.sender].push(_config);
+        bondNfts.push(nftAddress);
+        /*
         address nftAddress = bondNFTGenerator.generateNFT(
                                 _artistName, _artistId, _channelId, defaultEndpont, _audiusArtistId,
-                                _fundingAmount, _numberOfYears, _numberOfBonds, _facevalue);
-        requestToNFTAddress[requestId] = nftAddress;
-        
+                                _fundingAmount, _numberOfYears, _numberOfBonds, _facevalue,
+                                address(chainlinkOracleInfo), _bondName, _bondSymbol);
+
+        BondConfig memory _config = BondConfig(_artistName,_artistId,_channelId,defaultEndpont,_audiusArtistId,_fundingAmount, _numberOfYears, _numberOfBonds, msg.sender,_facevalue,0,nftAddress);
+        userBondConfigs[msg.sender].push(_config);
+        bondNfts.push(nftAddress);
+        */
     }
 
-    function requestChannelListeners(string memory _channelId, string memory _endpoint) public returns (bytes32 requestId)  {
-        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
-        
-        // Set the URL to perform the GET request on
-        //request.add("get", "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD");
-        //request.add("path", "USD");
-        //request.addInt("times", 100);
-        
-        request.add("id", _channelId);
-        request.add("endpoint", _endpoint);
-
-        // Sends the request
-        requestId = sendChainlinkRequestTo(oracle, request, fee);
-        emit ListenerRequestInitiated(requestId);
-        
-    }
-
-    function fulfill(bytes32 _requestId, uint256 _listeners) public recordChainlinkFulfillment(_requestId)
-    {
-        bondConfigs[_requestId].listeners = _listeners;
-        //calculateFacevalue(_requestId);
-        //BondConfig memory _bondConfig = bondConfigs[_requestId];
-        //DynamicNFT nft = new DynamicNFT(100,_bondConfig.channelId,_bondConfig.endpint, _bondConfig.listeners);
-        //address nftAddress = bondNFTGenerator.generateNFT(100,_bondConfig.channelId,_bondConfig.endpint, _bondConfig.listeners);
-        BondNFT bondNft = BondNFT(requestToNFTAddress[_requestId]);
-        bondNft.updateTotalListeners(_listeners);
-        emit RequestListenerFulfilled(_requestId, _listeners, requestToNFTAddress[_requestId]);
-    }
 
     // Seems to be not need now 
     /*
@@ -102,14 +74,4 @@ contract BondNFTManager is ChainlinkClient, Ownable {
         _bondConfig.faceValue = _faceValue;
     }*/
 
-    function stringToBytes32(string memory source) private pure returns (bytes32 result) {
-        bytes memory tempEmptyStringTest = bytes(source);
-        if (tempEmptyStringTest.length == 0) {
-            return 0x0;
-        }
-
-        assembly { // solhint-disable-line no-inline-assembly
-            result := mload(add(source, 32))
-        }
-    }
 }
