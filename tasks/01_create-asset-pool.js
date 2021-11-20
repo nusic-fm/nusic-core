5a3871ad28d403edeab081760a75ae57f102a3f7
zeeshanhanif <zeeshanhanif@gmail.com>
2021-11-20 23:59:45 +0500

    Added create asset pool task

---
+++ tasks/01_create-asset-pool.js
@@ -0,0 +1,13 @@
+
+task("create-asset-pool")
+  .addParam("contract","Contract Address")
+  .setAction(async taskArgs=>{
+    const contractAddress = taskArgs.contract;
+    const BondNFTManager = await ethers.getContractFactory("BondNFTManager");
+
+    const bondNFTManager = await BondNFTManager.attach(contractAddress);
+    const txt = await bondNFTManager.createAssetPool(15000);
+    
+    console.log("createAssetPool hash = ",txt.hash);
+    console.log("createAssetPool = ",txt);
+});
