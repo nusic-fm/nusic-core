a239d7b891757d663dbcc374cb8b1a114a3ac96c
zeeshanhanif <zeeshanhanif@gmail.com>
2021-11-23 10:08:30 +0500

    Added scripts for separately deploying and testing NFTManager and BondNFT

---
+++ scripts/03_deploy-init-nft-manager.ts
@@ -0,0 +1,25 @@
+import { BigNumber } from '@ethersproject/bignumber';
+import { ethers, run } from 'hardhat';
+import { BondNFTGenerator, BondNFTGenerator__factory, BondNFTManager, BondNFTManager__factory, ChainlinkMetadataRequest, ChainlinkMetadataRequest__factory, ChainlinkOracleInfo, ChainlinkOracleInfo__factory, RatingEngine, RatingEngine__factory } from '../typechain';
+
+// This is only needed when we want to deploy BondNFTManager separately
+async function main() {
+
+  const BondNFTManager:BondNFTManager__factory = await ethers.getContractFactory("BondNFTManager");
+  const bondNFTManager:BondNFTManager = await BondNFTManager.deploy();
+  await bondNFTManager.deployed();
+  console.log("BondNFTManager deployed to:", bondNFTManager.address);
+
+  // Need to replace address of RatingEngine,BondNFTGenerator,ChainlinkOracleInfo,ChainlinkMetadataRequest in this same order
+  await bondNFTManager.initialize("0xd2A3BF5f8B9F9fD1aF81b1BA47A896e562Aa28D1", "0x0F4763d3652aFB7d061909C5080ee9323b292540", "0xDAccf5a4636c745c796182dCA912546f15C80133", "0xEDBbe0A5f876aF88b26B1f0BD06A43dC33EaDf81");
+  console.log("BondNFTManager Initialized");
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ scripts/05_nft-manager-local-test.ts
@@ -0,0 +1,43 @@
+import { BigNumber } from '@ethersproject/bignumber';
+import { ethers, run } from 'hardhat';
+import { BondNFTManager, BondNFTManager__factory } from '../typechain';
+
+// Test NFTManager locally without calling initialize function of it 
+async function main() {
+
+  const [owner] = await ethers.getSigners();
+
+  const BondNFTManager:BondNFTManager__factory = await ethers.getContractFactory("BondNFTManager");
+  const bondNFTManager:BondNFTManager = await BondNFTManager.deploy();
+  await bondNFTManager.deployed();
+  console.log("BondNFTManager deployed to:", bondNFTManager.address);
+
+  const assetPoolTxt = await bondNFTManager.createAssetPool(BigNumber.from("2000"));
+  console.log("Asset pool txt.hash = ",assetPoolTxt.hash);
+  console.log("Asset pool txt = ",assetPoolTxt);
+
+  const assetPoolAddress = await bondNFTManager.allAssetPools(0);
+  console.log("assetPoolAddress = ",assetPoolAddress);
+
+  const assetPool = await bondNFTManager.userAssetPools(await owner.getAddress(),0);
+  console.log("assetPool = ",assetPool); 
+  
+
+  const txt = await bondNFTManager.issueBond("Howie B","howie_b","UCOmHUn--16B90oW2L6FRR3Ab",
+                            "audiusArtistId",BigNumber.from("1000"),BigNumber.from("3"),
+                            BigNumber.from("10"),BigNumber.from("2000"),"HowieBNFT","HNFT",
+                            assetPoolAddress);
+
+  console.log("BondNFTManager issueBond done ");
+  console.log("issueBond txt hash = ",txt.hash);
+  console.log("issueBond txt hash = ",txt);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
