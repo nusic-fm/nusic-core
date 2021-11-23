7a635292ff7e541dc4439bb4f84fa9809368bbba
zeeshanhanif <zeeshanhanif@gmail.com>
2021-11-23 10:10:10 +0500

    updated mint nft script

--- tasks/06_mint-nft.js
+++ tasks/06_mint-nft.js
@@ -8,26 +8,21 @@ task("mint-nft")
 
     const BondNFTManager = await ethers.getContractFactory("BondNFTManager");
     const bondNFTManager = await BondNFTManager.attach(nftManagerAddress);
-    
-    const BondNFT = await ethers.getContractFactory("BondNFT");
-    const bondNFT = await BondNFT.attach(nftBondAddress);
-    //console.log("Bond NFT = ",bondNFT);
-    
-    console.log("bond count = ", (await bondNFT.numberOfBonds()).toString())
-    
-    const txt = await bondNFT.tokenURI(0, {
-      from: owner.getAddress(),
-      gasLimit: 12500000,
-      gasPrice: 1000000000,
-    });
-    //console.log("txt hash = ",txt.hash);
-    console.log("txt = ",txt);
-    
-    
-
-    /*
     const txt = await bondNFTManager.mintNFTBond(nftBondAddress);
     console.log("txt.hash = ",txt.hash);
     console.log("txt = ",txt);
+    
+    /*
+    const BondNFT = await ethers.getContractFactory("BondNFT");
+    const bondNFT = await BondNFT.attach(nftBondAddress);
+    console.log("Bond NFT = ",bondNFT);
+    
+    console.log("bond count = ", (await bondNFT.numberOfBonds()).toString())
+    
+    const txt = await bondNFT.mintBonds();
+    console.log("txt hash = ",txt.hash);
+    console.log("txt = ",txt);
     */
+    
+    
 });
