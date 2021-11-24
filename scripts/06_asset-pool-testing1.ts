9e6d8b72053748bc5f862c4dbd8d1f92246a7540
zeeshanhanif <zeeshanhanif@gmail.com>
2021-11-24 23:49:32 +0500

    Added script to test separate creating and reading of asset pool

---
+++ scripts/06_asset-pool-testing1.ts
@@ -0,0 +1,49 @@
+import { BigNumber } from '@ethersproject/bignumber';
+import { ethers, run } from 'hardhat';
+import { AssetPool, AssetPool__factory } from '../typechain';
+
+// To Check AssetPool Deployment Separately
+async function main() {
+
+  const [owner] = await ethers.getSigners();
+  const AssetPool:AssetPool__factory = await ethers.getContractFactory("AssetPool");
+  
+  // Uncomment below two lines to deploy new AssetPool
+  
+  /*
+  const assetPool:AssetPool = await AssetPool.deploy();
+  await assetPool.deployed();
+  console.log("AssetPool deployed to:", assetPool.address);
+
+  const txt = await assetPool.initialize(await owner.getAddress(),BigNumber.from("2000"));
+  console.log("AssetPool Initialized");
+  console.log("AssetPool txt.hash =",txt.hash);
+  //console.log("AssetPool txt = ",txt);
+  */
+  // Uncomment only to attach existing AssetPool
+  const assetPool:AssetPool = await AssetPool.attach("0x610178dA211FEF7D417bC0e6FeD39F05609AD788");
+
+  const txt1 = await owner.sendTransaction({
+    to: assetPool.address,
+    value: ethers.utils.parseEther("0.0001")
+  });
+  await txt1.wait();
+  console.log("AssetPool Payment Received");
+  console.log("AssetPool txt1.hash =",txt1.hash);
+  //console.log("AssetPool txt1 = ",txt1);
+  
+  const balanceOfAssetPool:BigNumber = await ethers.provider.getBalance(assetPool.address);
+  console.log("Asset Pool Balance = ", balanceOfAssetPool.toString());
+
+
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ scripts/07_asset-pool-reading-testing2.ts
@@ -0,0 +1,30 @@
+import { BigNumber } from '@ethersproject/bignumber';
+import { ethers, run } from 'hardhat';
+import { AssetPool, AssetPool__factory } from '../typechain';
+
+// To Check AssetPool Deployment Separately
+async function main() {
+
+  const [owner] = await ethers.getSigners();
+  const AssetPool:AssetPool__factory = await ethers.getContractFactory("AssetPool");
+  
+  const assetPool:AssetPool = await AssetPool.attach("0x610178dA211FEF7D417bC0e6FeD39F05609AD788");
+
+  console.log("AssetPool deployed to:", assetPool.address);
+
+  const balanceOfAssetPool:BigNumber = await ethers.provider.getBalance(assetPool.address);
+  console.log("Asset Pool Balance = ", balanceOfAssetPool.toString());
+
+  console.log("Deposit check = ", (await assetPool.quarterCheckPoints(0)).toString());
+
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
