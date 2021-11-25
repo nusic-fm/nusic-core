351650166274ff9837a675bba3cae0fe131c82b4
zeeshanhanif <zeeshanhanif@gmail.com>
2021-11-26 01:11:41 +0500

    Added separate chainlink request contracts for youtube subscribers and spotify listeners

---
+++ contracts/ChainlinkRequest.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+
+
+abstract contract ChainlinkRequest is ChainlinkClient, Ownable {
+    using Strings for string;
+    using Chainlink for Chainlink.Request;
+
+    address internal oracle;
+    bytes32 internal jobId;
+    uint256 internal fee;
+
+    mapping(bytes32=>address) internal requestToSender;
+    mapping(bytes32=>address) internal requestToNFTAddress;
+
+    constructor() {
+        setPublicChainlinkToken();
+    }
+
+    function initialize(address _oracle, string memory _jobId, uint256 _fee) public virtual onlyOwner {
+        oracle = _oracle;
+        jobId = stringToBytes32(_jobId);
+        fee = _fee;
+        //fee = 1 * 10 ** 18; // (Varies by network and job)
+    }
+
+    function getOracle() public view returns (address) {
+        return oracle;
+    }
+
+    function getJobId() public view returns (bytes32) {
+        return jobId;
+    }
+
+    function getFee() public view returns (uint256) {
+        return fee;
+    }
+
+    function stringToBytes32(string memory source) private pure returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+
+        assembly { // solhint-disable-line no-inline-assembly
+            result := mload(add(source, 32))
+        }
+    }
+}
---
+++ contracts/ChainlinkYoutubeSubscribers.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./ChainlinkRequest.sol";
+import "./BondNFT.sol";
+
+
+contract ChainlinkYoutubeSubscribers is ChainlinkRequest{
+    using Strings for string;
+    using Chainlink for Chainlink.Request;
+
+    event YoutubeSubscribersRequestInitiated(bytes32 indexed _requestId, address indexed nftAddress);
+    event YoutubeSubscribersRequestFulfilled(bytes32 indexed _requestId, uint256 indexed _youtubeSubscribers, address indexed nftAddress);
+
+    function getLatestSubscribers(address _nftAddress) public returns (bytes32) {
+        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+        BondNFT _bondNFT = BondNFT(_nftAddress);
+        request.add("id", _bondNFT.channelId());
+        request.add("endpoint", _bondNFT.endpoint());
+
+        bytes32 requestId = sendChainlinkRequestTo(oracle, request, fee);
+        requestToSender[requestId] = msg.sender;
+        requestToNFTAddress[requestId] = _nftAddress;
+
+        emit YoutubeSubscribersRequestInitiated(requestId, address(this)); 
+        return requestId; 
+    }
+
+    function fulfill(bytes32 _requestId, uint256 _listeners) public recordChainlinkFulfillment(_requestId) {
+        address _nftAddress = requestToNFTAddress[_requestId];
+        BondNFT _bondNFT = BondNFT(_nftAddress);
+        _bondNFT.requestLatestYoutubeSubscribersFulFill(_requestId, _listeners);
+        emit YoutubeSubscribersRequestFulfilled(_requestId, _listeners, address(this));
+    }
+}
