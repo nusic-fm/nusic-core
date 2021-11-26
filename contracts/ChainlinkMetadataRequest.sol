7495a6c93bb001c2effcfb493acaa16a9be1f3a7
zeeshanhanif <zeeshanhanif@gmail.com>
2021-11-26 19:05:30 +0500

    Inherited ChainlinkMeatadataREquest from ChainlinkRequest to reuse common features

--- contracts/ChainlinkMetadataRequest.sol
+++ contracts/ChainlinkMetadataRequest.sol
@@ -1,47 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "./ChainlinkRequest.sol";
 import "./BondNFT.sol";
 
-contract ChainlinkMetadataRequest is ChainlinkClient, Ownable {
+contract ChainlinkMetadataRequest is ChainlinkRequest {
     using Strings for string;
     using Chainlink for Chainlink.Request;
 
-    address private oracle;
-    bytes32 private jobId;
-    uint256 private fee;
-
-    mapping(bytes32=>address) requestToSender;
-    mapping(bytes32=>address) requestToNFTAddress;
-
     event MetadataURIRequestInitiated(bytes32 indexed _requestId, address indexed nftAddress);
     event MetadataURIRequestFulfilled(bytes32 indexed _requestId, address indexed nftAddress, string indexed metadataURI);
-
-    constructor() {
-        setPublicChainlinkToken();
-    }
-
-    function initialize(address _oracle, string memory _jobId, uint256 _fee) public onlyOwner {
-        oracle = _oracle;
-        jobId = stringToBytes32(_jobId);
-        fee = _fee;
-        //fee = 1 * 10 ** 18; // (Varies by network and job)
-    }
-
-    function getOracle() public view returns (address) {
-        return oracle;
-    }
-
-    function getJobId() public view returns (bytes32) {
-        return jobId;
-    }
-
-    function getFee() public view returns (uint256) {
-        return fee;
-    }
 
     function getMetadataURI(address _nftAddress) public returns (bytes32) {
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
@@ -79,15 +47,5 @@ contract ChainlinkMetadataRequest is ChainlinkClient, Ownable {
             str[3+i*2] = alphabet[uint(uint8(data[i] & 0x0f))];
         }
         return string(str);
-    }
-
-    function stringToBytes32(string memory source) private pure returns (bytes32 result) {
-        bytes memory tempEmptyStringTest = bytes(source);
-        if (tempEmptyStringTest.length == 0) {
-            return 0x0;
-        }
-        assembly { // solhint-disable-line no-inline-assembly
-            result := mload(add(source, 32))
-        }
     }
 }
