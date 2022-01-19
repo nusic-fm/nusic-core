c3e509cd377d4cd18726d004b8f4ed2f956218a9
zeeshanhanif <zeeshanhanif@gmail.com>
2022-01-19 18:47:36 +0500

    Fixed naming issues test cases

--- test/test-before-any-round.ts
+++ test/test-before-any-round.ts
@@ -1,23 +1,23 @@
 import { BigNumber } from '@ethersproject/bignumber';
 import { expect } from 'chai';
 import { ethers, waffle } from 'hardhat';
-import { GNusic, GNusic__factory } from '../typechain';
+import { Nusic, Nusic__factory } from '../typechain';
 
 describe("Nusic NFT Deployed: Before any Investment round started", function () {
 
-  let gNusic:GNusic;
+  let nusic:Nusic;
   before(async()=>{
-    const GNusic:GNusic__factory =  await ethers.getContractFactory("GNusic");
-    gNusic = await GNusic.deploy("GNusic NFT","NUSIC","ipfs://QmXsMLpKjznF3z1KsVm5tNs3E94vj4BFAyAHvD5RTWgQ1J");
-    await gNusic.deployed();
+    const Nusic:Nusic__factory =  await ethers.getContractFactory("Nusic");
+    nusic = await Nusic.deploy("Nusic NFT","NUSIC","ipfs://QmXsMLpKjznF3z1KsVm5tNs3E94vj4BFAyAHvD5RTWgQ1J");
+    await nusic.deployed();
   });
 
   it("All Funding Rounds should in inActive", async function () {
     const [owner,addr1] = await ethers.getSigners();
-    console.log((await gNusic.connect(addr1).stage1Rounds(1)).isActive);
-    expect((await gNusic.connect(addr1).stage1Rounds(1)).isActive).to.be.false;
-    expect((await gNusic.connect(addr1).stage1Rounds(2)).isActive).to.be.false;
-    expect((await gNusic.connect(addr1).stage1Rounds(3)).isActive).to.be.false;
+    console.log((await nusic.connect(addr1).stage1Rounds(1)).isActive);
+    expect((await nusic.connect(addr1).stage1Rounds(1)).isActive).to.be.false;
+    expect((await nusic.connect(addr1).stage1Rounds(2)).isActive).to.be.false;
+    expect((await nusic.connect(addr1).stage1Rounds(3)).isActive).to.be.false;
   });
 
 });
