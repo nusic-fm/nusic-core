867de3ef0d56ed9e117f569ad41743ecc0f80564
zeeshanhanif <zeeshanhanif@gmail.com>
2022-01-24 22:00:35 +0500

    Added testcase for minting all pre-seed tokens and generate additional addresses

--- test/test-before-any-round.ts
+++ test/test-before-any-round.ts
@@ -1,15 +1,35 @@
 import { BigNumber } from '@ethersproject/bignumber';
+import { Wallet } from '@ethersproject/wallet';
 import { expect } from 'chai';
 import { ethers, waffle } from 'hardhat';
 import { Nusic, Nusic__factory } from '../typechain';
+var crypto = require('crypto');
 
 describe("Nusic NFT Deployed: Before any Investment round started", function () {
 
   let nusic:Nusic;
+  let _accountList:Wallet[] = [];
   before(async()=>{
+    const [owner,addr1] = await ethers.getSigners();
     const Nusic:Nusic__factory =  await ethers.getContractFactory("Nusic");
     nusic = await Nusic.deploy("Nusic NFT","NUSIC","ipfs://QmXsMLpKjznF3z1KsVm5tNs3E94vj4BFAyAHvD5RTWgQ1J");
     await nusic.deployed();
+
+    // Generate Accounts for Testing
+    const addressToBeGeneratedForPreSeed = (await nusic.MAX_PRE_SEED_SUPPLY()).div(await nusic.MINT_PER_TXT()).toNumber();
+    console.log("Pre-Seed Accounts Generated = ",addressToBeGeneratedForPreSeed);
+    
+    for(let i=0;i<addressToBeGeneratedForPreSeed;i++) {
+      var id = crypto.randomBytes(32).toString('hex');
+      var privateKey = "0x"+id;
+      var wallet = new ethers.Wallet(privateKey,ethers.provider);
+      _accountList.push(wallet);
+      // Transfering funds to new account as they will not have balance
+      await addr1.sendTransaction({
+        to:wallet.address,
+        value: ethers.utils.parseEther("1")
+      })
+    }
   });
 
   it("All Funding Rounds should in inActive", async function () {
@@ -44,8 +64,19 @@ describe("Nusic NFT Deployed: Before any Investment round started", function () 
 
   it("Pre-seed minting should even no round is active", async function () {
     const [owner,addr1] = await ethers.getSigners();
-    expect(await (nusic.connect(owner).preSeedMint(3, addr1.address))).to.be.ok;
-    expect(await (nusic.connect(owner).preSeedMinted())).to.be.equal(3);
+    expect(await (nusic.connect(owner).preSeedMint(5, addr1.address))).to.be.ok;
+    expect(await (nusic.connect(owner).preSeedMinted())).to.be.equal(5);
+  });
+
+  it("Pre-seed Minting should mint remaining token of Pre-seed supply", async function () {
+    const [owner,addr1] = await ethers.getSigners();
+
+    const mintCount = await nusic.MINT_PER_TXT(); 
+
+    for(let i=0; i<_accountList.length-1;i++){
+      expect(await nusic.connect(owner).preSeedMint(mintCount, _accountList[i].address)).to.be.ok;
+    }
+    expect(await (nusic.connect(owner).preSeedMinted())).to.be.equal(await nusic.MAX_PRE_SEED_SUPPLY());
   });
 
 });
