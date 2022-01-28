fbf2fc024b28b326142ce6d436bef64d34c9f585
zeeshanhanif <zeeshanhanif@gmail.com>
2022-01-29 01:47:40 +0500

    Added script to update price

---
+++ scripts/15_change-price.ts
@@ -0,0 +1,32 @@
+import { BigNumber } from 'ethers';
+import { ethers } from 'hardhat';
+import { Nusic, Nusic__factory } from '../typechain';
+/*
+* Main deployment script to deploy all the relevent contracts
+*/
+async function main() {
+  const [owner, add1] = await ethers.getSigners();
+  const Nusic:Nusic__factory = await ethers.getContractFactory("Nusic");
+  const nusic:Nusic = await Nusic.attach("0xc4B9A48176e352A62457C0f1BCd70b425D8451E8");
+  await nusic.deployed();
+  console.log("Nusic Address:", nusic.address);
+
+  
+  const txt1 = await nusic.setPrice(await ethers.utils.parseEther("0.01"));
+  console.log("Trasaction Hash = ",txt1.hash);
+  const receipt = await txt1.wait();
+  console.log("Receipt = ",receipt);
+  
+  //const price = await nusic.price();
+  //console.log("Price = ",price.toString());
+  
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
